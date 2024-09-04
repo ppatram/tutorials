e07ec6020436ca65cd3db70c5db85fa1dc3f7a3f
Your Name <you@example.com>
2024-09-04 19:28:04 +0000

    last stuff on github

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,4 +5,12 @@ echo 'root:your_password' | chpasswd
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 sed -i 's/^#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
+
+# setup keys
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInPjqseIUNlEKnhuwulg0zHwnuSNgzDm9JEgfenGCzx root@ubuntu-vdi
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXSNO8OPYiZC4FoPWlMnfp/MltK+Ski0xA4kwtjf6mC desky@ubuntu-vdi
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxGteolV2QkaOgBnwO8Ktuf3rmUlZHPBIvmyCnbhusH dickypatram@ubuntu-vdi' >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
 nohup /usr/sbin/sshd -D
